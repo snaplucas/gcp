0ef0c64892f6720fdc145f2141d0ffc9a91da6bc
snaplucas <lucas.chapola@gmail.com>
2018-05-28 23:22:12 -0300

    +swagger

--- src/main/kotlin/gcp/AppStart.kt
+++ src/main/kotlin/gcp/AppStart.kt
@@ -2,8 +2,10 @@ package gcp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class AppStart
 
 fun main(args: Array<String>) {
