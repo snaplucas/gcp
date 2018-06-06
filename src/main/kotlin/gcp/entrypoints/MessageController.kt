7ff13553e0f9138e7f4ba18f14287ae18534c9cb
snaplucas <lucas.chapola@gmail.com>
2018-06-05 22:04:17 -0300

    minor refactor

--- src/main/kotlin/gcp/entrypoints/MessageController.kt
+++ src/main/kotlin/gcp/entrypoints/MessageController.kt
@@ -1,6 +1,5 @@
 package gcp.entrypoints
 
-import gcp.domain.model.Message
 import gcp.domain.model.MessageService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 class MessageController(private val messageService: MessageService) {
 
     @GetMapping("/messages")
-    fun message(): List<Message> {
-        return messageService.getMessages()
-    }
+    fun message() = messageService.getMessages()
 
 }
